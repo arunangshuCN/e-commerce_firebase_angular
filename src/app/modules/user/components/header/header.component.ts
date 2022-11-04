de4b6e9eb19eab6c9d470333fcfb971378d91981
CN11 <cn11@CN11s-Mac-mini.local>
2022-11-04 19:12:15 +0530

    Adding logout auth

--- src/app/modules/user/components/header/header.component.ts
+++ src/app/modules/user/components/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit,OnDestroy {
 
-  constructor() { }
+  private userSub!:Subscription;
+  isAuthenticated=false;
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
+    this.userSub=this.authService.user.subscribe(user=>{
+      this.isAuthenticated=!!user
+      console.log(user);
+      
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe()
   }
 
 }
